a341a29c3a01acb698f196d8dbf92efcc009e57c
elkmare <30569445+elkmare@users.noreply.github.com>
2017-10-11 21:11:52 +1100

    Initialize web (#2)

---
+++ web/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Please wait');
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1 className="App-title">welcome to malusers</h1>
+        </header>
+
+        <div className="App-intro">
+            <form onSubmit={this.handleSubmit} >
+            <input type="text" placeholder="User name" value={this.state.value} onChange={this.handleChange}/>
+            <input type="submit" value="Submit"/>
+            </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
