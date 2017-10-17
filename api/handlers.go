72b00c496ed5f904d5df3bfdf30ef3fe85a63475
Modrzew <dev@modriv.net>
2017-10-17 23:36:24 +1100

    Simple API cache and logging

--- api/handlers.go
+++ api/handlers.go
@@ -5,43 +5,26 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
-	"github.com/modrzew/malusers"
 )
 
 // Handlers contains reference to the database and all handlers
 type Handlers struct {
-	DB *gorm.DB
+	DB    *gorm.DB
+	Cache *Cache
 }
 
 // GetUserStats returns JSON info about single user
 func (h *Handlers) GetUserStats(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	user := malusers.User{}
 	username := strings.ToLower(params["username"])
-	h.DB.Where("username = ?", username).First(&user)
-	if h.DB.NewRecord(user) {
+	stats, error := h.Cache.GetUser(username)
+	if error != nil {
 		w.WriteHeader(404)
 		fmt.Fprint(w, "404 not found")
 		return
-	}
-	h.DB.Where("username = ?", username).Find(&user.AnimeStats)
-	h.DB.Where("username = ?", username).Find(&user.MangaStats)
-	h.DB.Where("username = ?", username).Find(&user.Ranking)
-	age := time.Since(user.Birthday.Time).Hours() / 24 / 365
-	var count int
-	h.DB.Model(&malusers.User{}).Count(&count)
-	stats := UserStats{
-		Username:        user.DisplayName,
-		Age:             int(age),
-		LastUpdate:      user.UpdatedAt.UTC(),
-		AnimeStats:      user.AnimeStats,
-		MangaStats:      user.MangaStats,
-		Ranking:         DBRankingToSchema(user.Ranking),
-		TotalUsersCount: count,
 	}
 	json.NewEncoder(w).Encode(stats)
 }
--- cmd/malapi/main.go
+++ cmd/malapi/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -10,17 +12,31 @@ import (
 	"github.com/modrzew/malusers/api"
 )
 
-func addHandlers(router *mux.Router, db *gorm.DB) {
-	handlers := &api.Handlers{DB: db}
-	router.HandleFunc("/user/{username}", handlers.GetUserStats).Methods("GET")
+func logger(inner http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		inner(w, r)
+		log.Printf(
+			"%s\t%s\t%s",
+			r.Method,
+			r.RequestURI,
+			time.Since(start),
+		)
+	})
+}
+
+func addHandlers(router *mux.Router, db *gorm.DB, cache *api.Cache) {
+	handlers := &api.Handlers{DB: db, Cache: cache}
+	router.Handle("/user/{username}", logger(handlers.GetUserStats)).Methods("GET")
 }
 
 func main() {
 	config := malusers.ReadConfig()
 	db := malusers.OpenDb()
+	cache := api.GetCache(db)
 
 	router := mux.NewRouter()
-	addHandlers(router, db)
+	addHandlers(router, db, cache)
 	url := fmt.Sprintf("%s:%d", config.API.Host, config.API.Port)
 	http.ListenAndServe(url, router)
 }
