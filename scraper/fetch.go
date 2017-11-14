5342d39321cecdb36cb640e9ad2cb8576e24b9c3
Modrzew <dev@modriv.net>
2017-11-14 21:44:34 +1100

    Remove debug code

--- scraper/fetch.go
+++ scraper/fetch.go
@@ -68,8 +68,6 @@ func getFriends(channel chan []string, username string, offset int) {
 
 // GetUser obtains stats for single user and their friends
 func GetUser(username string, db *gorm.DB, finished chan bool) {
-	// username = "sweetmonia"
-	fmt.Println("Getting", username)
 	user := GetOrCreateUser(username, db)
 	if user.Fetched {
 		return
